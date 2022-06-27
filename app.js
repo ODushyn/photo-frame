aff6a012465a47a0ec842983a0d94eca62368805
odushyn <oledus@redpill.linpro>
2022-06-27 23:17:51 +0200

    center frame

--- app.js
+++ app.js
@@ -3,14 +3,15 @@
 
 import bodyParser from 'body-parser';
 import express from 'express';
+import fetch from 'node-fetch';
 import http from 'http';
 import passport from 'passport';
 import session from 'express-session';
 import sessionFileStore from 'session-file-store';
 
-import {auth} from './auth.js';
-import {config} from './config.js';
-import {fileURLToPath} from 'url';
+import { auth } from './auth.js';
+import { config } from './config.js';
+import { fileURLToPath } from 'url';
 
 const app = express();
 const fileStore = sessionFileStore(session);
@@ -19,17 +20,17 @@ const server = http.Server(app);
 auth(passport);
 
 const sessionMiddleware = session({
-    resave: true,
-    saveUninitialized: true,
-    store: new fileStore({}),
-    secret: 'photo frame sample',
+  resave: true,
+  saveUninitialized: true,
+  store: new fileStore({}),
+  secret: 'photo frame sample',
 });
 
 app.set('view engine', 'ejs');
 
 app.use(sessionMiddleware);
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(passport.initialize());
 app.use(passport.session());
@@ -44,40 +45,77 @@ app.use('/js',
 );
 
 app.get('/', (req, res) => {
-    if (!req.user || !req.isAuthenticated()) {
-      // Not logged in yet.
-      res.render('pages/login');
-    } else {
-      res.render('pages/frame');
-    }
+  if (!req.user || !req.isAuthenticated()) {
+    // Not logged in yet.
+    res.render('pages/login');
+  } else {
+    res.render('pages/frame');
+  }
 });
 
 app.get('/logout', (req, res) => {
-    req.logout();
-    req.session.destroy();
-    res.redirect('/');
+  req.logout();
+  req.session.destroy();
+  res.redirect('/');
 });
 
 app.get('/auth/google', passport.authenticate('google', {
-    scope: config.scopes,
-    failureFlash: true,
-    session: true,
+  scope: config.scopes,
+  failureFlash: true,
+  session: true,
 }));
 
 app.get(
-    '/auth/google/callback',
-    passport.authenticate(
-        'google', {failureRedirect: '/', failureFlash: true, session: true}),
-    (req, res) => {
-      // User has logged in.
-      console.log('User has logged in.');
-      req.session.save(() => {
-        res.redirect('/');
-      });
-});
+  '/auth/google/callback',
+  passport.authenticate(
+    'google', { failureRedirect: '/', failureFlash: true, session: true }),
+  (req, res) => {
+    // User has logged in.
+    console.log('User has logged in.');
+    req.session.save(() => {
+      res.redirect('/');
+    });
+  });
+
+app.get('/getQueue', async (req, res) => {
+  const userId = req.user.profile.id;
+  const authToken = req.user.token;
+  console.log(userId);
+  console.log(authToken);
+  const data = await libraryApiSearch(authToken);
+
+  return res.status(200).send(data);
+})
+
+async function libraryApiSearch(authToken) {
+  let photos = [];
+  let nextPageToken = null;
+  let error = null;
+  const parameters = {
+    pageSize: config.searchPageSize
+  }
+
+  console.log(
+    `Submitting search with parameters: ${JSON.stringify(parameters)}`);
+
+  // Make a POST request to search the library or album
+  const searchResponse =
+    await fetch(config.apiEndpoint + '/v1/mediaItems:search', {
+      method: 'post',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + authToken
+      },
+      body: JSON.stringify(parameters)
+    });
+
+  const result = await searchResponse.json();
+
+  return result;
+}
 
 // Start the server
 server.listen(config.port, () => {
-    console.log(`App listening on port ${config.port}`);
-    console.log('Press Ctrl+C to quit.');
+  console.log(`App listening on port ${config.port}`);
+  console.log('Press Ctrl+C to quit.');
 });
--- static/js/frame.js
+++ static/js/frame.js
@@ -1,10 +1,26 @@
 
 function showPreview(mediaItems) {
-    const fullUrl = "https://lh3.googleusercontent.com/pkrOkrBgML52L1B97tRRDMCt0wKeuqNDen9tFNUir9921QlnzkBvIEDx3q-bFMMUgsqKxkN-69GxciSOHwzpYslpZTUAU5mgGivulFcunYAEQCH_Zz3gIuTGy2SxZS_nDeYWcQEwT6o8SLqe_5-qmWdWPdye1MwruGVwNH0nga6mtlbFrzNUamCA2UIb1oQIZdawjm85VZag5xREzFdCWTcr8ImI4UQqg5uX3rM53jfM5NGwfrfjKkSxKoMXWbMD60cnbdIiOURJJlNmtyLlgIJqA8xVeYeZ21GTiq3-Iqri2gfsQQBm-K_PRiiZcj-NNHkAcvRnGIql5lMN2YnCBbFDa3DlodbeyRBW5dGQZV_0tEWUhqzPJtvQSIQq6-qZqwQeGwxTD67goedUeMdXVGP1kmAGnS9QruncpCKh8xZd6LfXvc-tQTM5OVHJzt9p1IrjluNkUAALdwZoCt5M8LPOLLJDzduYYL3Q5dqKgU9fALBEe0up6e5XpoEZl6J4XdSOp4_b_WzLo0sl22fD1S12H35BjiYvnl-FNccEJ2fc69ctLPvMKVANtY_xAPbcRLVHiq4bh8TmKUtnMsN-CbuKUP7iUcoxZmhR97KB61XNt7gmVDs6hqB0BgfHvqCia0pLXZ65B0d3eP-2jT6aEwaWmccMPArUrhZiVDmLMZ7kxVlPdHdzPPvbrymIWyQ0y0LX1UzYY9HCaMCOrG4h7GF-TNmmw8wWk3d1ptn2d_ehFO7_rMq0GOBtjmVqagk=w747-h1328-no?authuser=0";
-    const linkToFullImage = $('<img />')
-        .attr('src', fullUrl)
-    console.log(linkToFullImage);
-    $('#images-container').append(linkToFullImage);
+    $.ajax({
+        type: 'GET',
+        url: '/getQueue',
+        dataType: 'json',
+        success: (data) => {
+
+            console.log("Success from html");
+            console.log(data.mediaItems);
+            const item = data.mediaItems[0];
+            const fullUrl = `${item.baseUrl}=w${item.mediaMetadata.width}-h${
+                item.mediaMetadata.height}`;
+            const linkToFullImage = $('<img />')
+                .attr('src', fullUrl)
+                .addClass('center-fit')
+            $('#images-container').append(linkToFullImage);
+
+        },
+        error: (data) => {
+            console.log('Could not load queue', data)
+        }
+    });
 }
 
 
