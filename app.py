dcfc4274c77378d22b245dcb3e0ae7e2d11d77ff
odushyn <oleksandr.dushyn@gmail.com>
2023-09-08 22:13:51 +0200

    disable warning about browser controlled by test software

--- app.py
+++ app.py
@@ -184,12 +184,13 @@ def album_exists(album, fetched_albums):
 
 
 def get_browser_driver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument("--disable-gpu")
     chrome_options.add_argument("--no-sandbox")
     chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--remote-debugging-port=9222")
     chrome_options.add_argument("--start-fullscreen")
+    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
     chrome_driver = webdriver.Chrome(options=chrome_options)
     return chrome_driver
 
